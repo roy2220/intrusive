37906f23b95395199ab4fe84c16f04b327e5bdae
Roy O'Young <roy2220@outlook.com>
2020-04-02 23:28:51 +0800

    Implement list

---
+++ list.go
@@ -0,0 +1,254 @@
+package intrusive
+
+import "unsafe"
+
+// List presents a doubly-linked list.
+type List struct {
+	nil ListNode
+}
+
+// Init initializes the list and then returns the list.
+func (l *List) Init() *List {
+	l.nil = ListNode{&l.nil, &l.nil}
+	return l
+}
+
+// AppendNode inserts the given node at the end of the list.
+// The given node must be detatched from any list.
+func (l *List) AppendNode(node *ListNode) {
+	node.insert(l.Tail(), &l.nil)
+}
+
+// PrependNode inserts the given node at the beginning of the list.
+// The given node must be detatched from any list.
+func (l *List) PrependNode(node *ListNode) {
+	node.insert(&l.nil, l.Head())
+}
+
+// AppendNodes removes all nodes of the given other list and then inserts
+// the nodes at the end of the list.
+func (l *List) AppendNodes(other *List) {
+	if other.IsEmpty() {
+		return
+	}
+
+	insertListSlice(other.Head(), other.Tail(), l.Tail(), &l.nil)
+	other.Init()
+}
+
+// PrependNodes removes all nodes of the given other list and then inserts
+// the nodes at the beginning of the list.
+func (l *List) PrependNodes(other *List) {
+	if other.IsEmpty() {
+		return
+	}
+
+	insertListSlice(other.Head(), other.Tail(), &l.nil, l.Head())
+	other.Init()
+}
+
+// AppendSlice inserts the given slice at the end of the list.
+// The given slice must be detatched from any list.
+func (l *List) AppendSlice(firstNode *ListNode, lastNode *ListNode) {
+	insertListSlice(firstNode, lastNode, l.Tail(), &l.nil)
+}
+
+// PrependSlice inserts the given slice at the beginning of the list.
+// The given slice must be detatched from any list.
+func (l *List) PrependSlice(firstNode *ListNode, lastNode *ListNode) {
+	insertListSlice(firstNode, lastNode, &l.nil, l.Head())
+}
+
+// GetNodes returns an iterator over all nodes of the list in order.
+func (l *List) GetNodes() ListIterator {
+	iterator := forwardListIterator{}
+	iterator.init(l)
+	return &iterator
+}
+
+// GetReverseNodes returns an iterator over all nodes of the list in
+// reverse order.
+func (l *List) GetReverseNodes() ListIterator {
+	iterator := backwardListIterator{}
+	iterator.init(l)
+	return &iterator
+}
+
+// IsEmpty indicates whether the list is empty.
+func (l *List) IsEmpty() bool {
+	return l.Tail() == &l.nil
+}
+
+// Tail returns the last node of the list.
+// The last node can be null (using *ListNode.IsNull to test)
+// when the list is empty.
+func (l *List) Tail() *ListNode {
+	return l.nil.prev
+}
+
+// Head returns the first node of the list.
+// The first node can be null (using *ListNode.IsNull to test)
+// when the list is empty.
+func (l *List) Head() *ListNode {
+	return l.nil.next
+}
+
+// ListNode represents a node in a doubly-linked list.
+type ListNode struct {
+	prev, next *ListNode
+}
+
+// InsertBefore inserts the node before the given other node.
+// The node must be detatched from any list.
+// Inserting the node before a null node is legal as if inserting
+// at the end of a list.
+func (ln *ListNode) InsertBefore(other *ListNode) {
+	ln.insert(other.prev, other)
+}
+
+// InsertAfter inserts the node after the given other node.
+// The node must be detatched from any list.
+// Inserting the node after a null node is legal as if inserting
+// at the beginning of a list.
+func (ln *ListNode) InsertAfter(other *ListNode) {
+	ln.insert(other, other.next)
+}
+
+// Remove removes the node from a list.
+// The node must be attached to a list.
+func (ln *ListNode) Remove() {
+	ln.prev.setNext(ln.next)
+}
+
+// GetContainer returns a pointer to the container which contains
+// the ListNode field about the node.
+// The given offset is of the ListNode field in the container.
+func (ln *ListNode) GetContainer(offset uintptr) unsafe.Pointer {
+	return unsafe.Pointer(uintptr(unsafe.Pointer(ln)) - offset)
+}
+
+// IsNull indicates whether the node is null (the nil of the given list).
+func (ln *ListNode) IsNull(l *List) bool {
+	return ln == &l.nil
+}
+
+// IsNull indicates whether the node is reset (to a zero value).
+func (ln *ListNode) IsReset() bool {
+	return ln.prev == nil
+}
+
+// Prev returns the previous node of the node.
+// The previous node can be null (using *ListNode.IsNull to test)
+// when the node is at the beginning of a list.
+func (ln *ListNode) Prev() *ListNode {
+	return ln.prev
+}
+
+// Next returns the next node of the node.
+// The next node can be null (using *ListNode.IsNull to test)
+// when the node is at the end of a list.
+func (ln *ListNode) Next() *ListNode {
+	return ln.next
+}
+
+func (ln *ListNode) insert(prev *ListNode, next *ListNode) {
+	ln.setPrev(prev)
+	ln.setNext(next)
+}
+
+func (ln *ListNode) setPrev(prev *ListNode) {
+	ln.prev = prev
+	prev.next = ln
+}
+
+func (ln *ListNode) setNext(next *ListNode) {
+	ln.next = next
+	next.prev = ln
+}
+
+// ListIterator represents an iteration over all nodes in a list.
+type ListIterator interface {
+	// IsAtEnd indicates whether the iteration has no more nodes.
+	IsAtEnd() bool
+
+	// Node returns the current node in the iteration.
+	Node() *ListNode
+
+	// Advance advances the iteration to the next node.
+	Advance()
+}
+
+// InsertListSliceBefore inserts the given slice before given list node.
+// The given slice must be detached from any list.
+// The given node must be attached to a list.
+func InsertListSliceBefore(firstListNode *ListNode, lastListNode *ListNode, listNode *ListNode) {
+	insertListSlice(firstListNode, lastListNode, listNode.prev, listNode)
+}
+
+// InsertListSliceAfter inserts the given slice after given list node.
+// The given slice must be detached from any list.
+// The given node must be attached to a list.
+func InsertListSliceAfter(firstListNode *ListNode, lastListNode *ListNode, listNode *ListNode) {
+	insertListSlice(firstListNode, lastListNode, listNode, listNode.next)
+}
+
+// RemoveListSlice removes the given slice from a list.
+// The given slice must be attached to a list.
+func RemoveListSlice(firstListNode *ListNode, lastListNode *ListNode) {
+	firstListNode.prev.setNext(lastListNode.next)
+}
+
+type forwardListIterator struct {
+	listIterator
+}
+
+var _ = (ListIterator)((*forwardListIterator)(nil))
+
+func (fli *forwardListIterator) init(l *List) {
+	fli.l = l
+	fli.node = l.Head()
+	fli.nextNode = fli.node.Next()
+}
+
+func (fli *forwardListIterator) Advance() {
+	fli.advance(fli.nextNode.Next())
+}
+
+type backwardListIterator struct {
+	listIterator
+}
+
+var _ = (ListIterator)((*backwardListIterator)(nil))
+
+func (bli *backwardListIterator) init(l *List) {
+	bli.l = l
+	bli.node = l.Tail()
+	bli.nextNode = bli.node.Prev()
+}
+
+func (bli *backwardListIterator) Advance() {
+	bli.advance(bli.nextNode.Prev())
+}
+
+type listIterator struct {
+	l              *List
+	node, nextNode *ListNode
+}
+
+func (li *listIterator) IsAtEnd() bool {
+	return li.node.IsNull(li.l)
+}
+
+func (li *listIterator) Node() *ListNode {
+	return li.node
+}
+
+func (li *listIterator) advance(nextNode *ListNode) {
+	li.node = li.nextNode
+	li.nextNode = nextNode
+}
+
+func insertListSlice(firstListNode *ListNode, lastListNode *ListNode, firstListNodePrev *ListNode, lastListNodeNext *ListNode) {
+	firstListNode.setPrev(firstListNodePrev)
+	lastListNode.setNext(lastListNodeNext)
+}
---
+++ list_test.go
@@ -0,0 +1,194 @@
+package intrusive_test
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+	"unsafe"
+
+	"github.com/roy2220/intrusive"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestListInsertNode(t *testing.T) {
+	for i, tt := range []struct {
+		In  func(*intrusive.List) intrusive.ListIterator
+		Out string
+	}{
+		{
+			In: func(l *intrusive.List) intrusive.ListIterator {
+				l.PrependNodes(new(intrusive.List).Init())
+				l.AppendNodes(new(intrusive.List).Init())
+				return l.GetNodes()
+			},
+			Out: "",
+		},
+		{
+			In: func(l *intrusive.List) intrusive.ListIterator {
+				l.AppendNode(&(&recordOfList{Value: 1}).ListNode)
+				l.PrependNode(&(&recordOfList{Value: 2}).ListNode)
+				(&recordOfList{Value: 3}).ListNode.InsertAfter(l.Head())
+				(&recordOfList{Value: 4}).ListNode.InsertBefore(l.Tail())
+				(&recordOfList{Value: 5}).ListNode.InsertAfter(l.Tail())
+				(&recordOfList{Value: 6}).ListNode.InsertBefore(l.Head())
+				return l.GetNodes()
+			},
+			Out: "6,2,3,4,1,5",
+		},
+		{
+			In: func(l *intrusive.List) intrusive.ListIterator {
+				for i := 0; i < 3; i++ {
+					l.AppendNode(&(&recordOfList{Value: i + 1}).ListNode)
+				}
+				l2 := new(intrusive.List).Init()
+				for i := 3; i < 6; i++ {
+					l2.AppendNode(&(&recordOfList{Value: i + 1}).ListNode)
+				}
+				l.AppendNodes(l2)
+				return l.GetNodes()
+			},
+			Out: "1,2,3,4,5,6",
+		},
+		{
+			In: func(l *intrusive.List) intrusive.ListIterator {
+				for i := 0; i < 3; i++ {
+					l.AppendNode(&(&recordOfList{Value: i + 1}).ListNode)
+				}
+				l2 := new(intrusive.List).Init()
+				for i := 3; i < 6; i++ {
+					l2.AppendNode(&(&recordOfList{Value: i + 1}).ListNode)
+				}
+				l.AppendSlice(l2.Head(), l2.Tail())
+				return l.GetNodes()
+			},
+			Out: "1,2,3,4,5,6",
+		},
+		{
+			In: func(l *intrusive.List) intrusive.ListIterator {
+				for i := 0; i < 3; i++ {
+					l.PrependNode(&(&recordOfList{Value: i + 1}).ListNode)
+				}
+				l2 := new(intrusive.List).Init()
+				for i := 3; i < 6; i++ {
+					l2.PrependNode(&(&recordOfList{Value: i + 1}).ListNode)
+				}
+				l.PrependNodes(l2)
+				assert.True(t, l2.IsEmpty())
+				return l.GetReverseNodes()
+			},
+			Out: "1,2,3,4,5,6",
+		},
+		{
+			In: func(l *intrusive.List) intrusive.ListIterator {
+				for i := 0; i < 3; i++ {
+					l.PrependNode(&(&recordOfList{Value: i + 1}).ListNode)
+				}
+				l2 := new(intrusive.List).Init()
+				for i := 3; i < 6; i++ {
+					l2.PrependNode(&(&recordOfList{Value: i + 1}).ListNode)
+				}
+				l.PrependSlice(l2.Head(), l2.Tail())
+				return l.GetReverseNodes()
+			},
+			Out: "1,2,3,4,5,6",
+		},
+		{
+			In: func(l *intrusive.List) intrusive.ListIterator {
+				for i := 0; i < 3; i++ {
+					l.AppendNode(&(&recordOfList{Value: i + 1}).ListNode)
+				}
+				l2 := new(intrusive.List).Init()
+				for i := 3; i < 6; i++ {
+					l2.AppendNode(&(&recordOfList{Value: i + 1}).ListNode)
+				}
+				intrusive.InsertListSliceAfter(l2.Head(), l2.Tail(), l.Head())
+				return l.GetNodes()
+			},
+			Out: "1,4,5,6,2,3",
+		},
+		{
+			In: func(l *intrusive.List) intrusive.ListIterator {
+				for i := 0; i < 3; i++ {
+					l.AppendNode(&(&recordOfList{Value: i + 1}).ListNode)
+				}
+				l2 := new(intrusive.List).Init()
+				for i := 3; i < 6; i++ {
+					l2.AppendNode(&(&recordOfList{Value: i + 1}).ListNode)
+				}
+				intrusive.InsertListSliceBefore(l2.Head(), l2.Tail(), l.Tail())
+				return l.GetNodes()
+			},
+			Out: "1,2,4,5,6,3",
+		},
+	} {
+		l := new(intrusive.List).Init()
+		assert.Equal(t, tt.Out, dumpRecordList(tt.In(l)), "case %d", i)
+	}
+}
+
+func TestListRemoveNode(t *testing.T) {
+	for i, tt := range []struct {
+		In  func(*intrusive.List) intrusive.ListIterator
+		Out string
+	}{
+		{
+			In:  func(l *intrusive.List) intrusive.ListIterator { return l.GetNodes() },
+			Out: "1,2,3,4,5,6",
+		},
+		{
+			In: func(l *intrusive.List) intrusive.ListIterator {
+				l.Head().Remove()
+				l.Tail().Remove()
+				l.Head().Next().Remove()
+				l.Tail().Prev().Remove()
+				return l.GetNodes()
+			},
+			Out: "2,5",
+		},
+		{
+			In: func(l *intrusive.List) intrusive.ListIterator {
+				x, y := l.Head().Next(), l.Tail().Prev()
+				intrusive.RemoveListSlice(x, y)
+				return l.GetReverseNodes()
+			},
+			Out: "6,1",
+		},
+		{
+			In: func(l *intrusive.List) intrusive.ListIterator {
+				x, y := l.Head(), l.Tail()
+				intrusive.RemoveListSlice(x, y)
+				return l.GetNodes()
+			},
+			Out: "",
+		},
+	} {
+		l := new(intrusive.List).Init()
+		for i := 0; i < 6; i++ {
+			r := recordOfList{Value: i + 1}
+			assert.True(t, r.ListNode.IsReset())
+			l.AppendNode(&r.ListNode)
+		}
+		assert.Equal(t, tt.Out, dumpRecordList(tt.In(l)), "case %d", i)
+	}
+}
+
+type recordOfList struct {
+	Value    int
+	ListNode intrusive.ListNode
+}
+
+func dumpRecordList(it intrusive.ListIterator) string {
+	buffer := bytes.NewBuffer(nil)
+
+	for ; !it.IsAtEnd(); it.Advance() {
+		record := (*recordOfList)(it.Node().GetContainer(unsafe.Offsetof(recordOfList{}.ListNode)))
+		fmt.Fprintf(buffer, "%v,", record.Value)
+	}
+
+	if n := buffer.Len(); n >= 1 {
+		buffer.Truncate(n - 1)
+		return buffer.String()
+	}
+
+	return ""
+}
